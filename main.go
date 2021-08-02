7cbe3046170a469df97f6fb0db178d24d923882e
Jef Spaleta <jspaleta@gmail.com>
2021-08-02 12:25:23 -0800

    refactor with comments

--- main.go
+++ main.go
@@ -3,18 +3,26 @@ package main
 import (
 	"fmt"
 	"log"
+	// FIXME: Replace s3 with the correct aws service subpackage from aws-sdk-go-v2
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 
+	"github.com/sensu-community/sensu-plugin-sdk/aws"
 	"github.com/sensu-community/sensu-plugin-sdk/sensu"
 	"github.com/sensu/sensu-go/api/core/v2"
 )
 
 // Config represents the check plugin config.
 type Config struct {
+	//Base Sensu plugin configs
 	sensu.PluginConfig
+	//AWS specific Sensu plugin configs
+	aws.AWSPluginConfig
+	//Additional configs for this check command
 	Example string
 }
 
 var (
+	//initialize Sensu plugin Config object
 	plugin = Config{
 		PluginConfig: sensu.PluginConfig{
 			Name:     "{{ .GithubProject }}",
@@ -22,7 +30,7 @@ var (
 			Keyspace: "sensu.io/plugins/{{ .GithubProject }}/config",
 		},
 	}
-
+	//initialize options list with custom options
 	options = []*sensu.PluginConfigOption{
 		&sensu.PluginConfigOption{
 			Path:      "example",
@@ -36,12 +44,23 @@ var (
 	}
 )
 
+func init() {
+	//append AWS options to options list
+	options = append(options, plugin.GetAWSOpts()...)
+}
+
 func main() {
 	check := sensu.NewGoCheck(&plugin.PluginConfig, options, checkArgs, executeCheck, false)
 	check.Execute()
 }
 
 func checkArgs(event *v2.Event) (int, error) {
+	// Check for valid AWS credentials
+	if state, err := plugin.CheckAWSCreds(); err != nil {
+		return state, err
+	}
+
+	// Specific Argument Checking for this command
 	if len(plugin.Example) == 0 {
 		return sensu.CheckStateWarning, fmt.Errorf("--example or CHECK_EXAMPLE environment variable is required")
 	}
@@ -49,6 +68,41 @@ func checkArgs(event *v2.Event) (int, error) {
 }
 
 func executeCheck(event *v2.Event) (int, error) {
-	log.Println("executing check with --example", plugin.Example)
+	//Make sure plugin.CheckAwsCreds() worked as expected
+	if plugin.AWSConfig == nil {
+		return sensu.CheckStateCritical, fmt.Errorf("AWS Config undefined, something went wrong in processing AWS configuration information")
+	}
+	//Start AWS Service specific client
+	// FIXME: replace s3 with correct service from AWS SDK
+	client := s3.NewFromConfig(*plugin.AWSConfig)
+	//Run business logic for check
+	state, err := checkFunction(client)
+	return state, err
+}
+
+//Create service interface to help with mock testing
+// FIXME: replace s3 functions with correct service functions from AWS SDK
+type ServiceAPI interface {
+	ListBuckets(ctx context.Context, params *s3.ListBucketsInput, optFns ...func(*s3.Options)) (*s3.ListBucketsOutput, error)
+	GetBucketTagging(ctx context.Context, params *s3.GetBucketTaggingInput, optFns ...func(*s3.Options)) (*s3.GetBucketTaggingOutput, error)
+}
+
+// Note: Use interface definition to make function testable with mock API testing pattern
+// FIXME: replace s3 with correct service from AWS SDK
+func checkFunction(client ServiceAPI) (int, error) {
+	inputs := &s3.ListBucketsInput{}
+	output, err := client.ListBuckets(context.Background(), inputs)
+	if err != nil {
+		return sensu.CheckStateCritical, err
+	}
+	if output != nil && output.Buckets != nil && len(output.Buckets) > 0 {
+		for _, bucket := range output.Buckets {
+			bucketInput := &s3.GetBucketTaggingInput{Bucket: bucket.Name}
+			bucketOutput, err := client.GetBucketTagging(context.Background(), bucketInput)
+			if err != nil {
+				continue
+			}
+		}
+	}
 	return sensu.CheckStateOK, nil
 }
